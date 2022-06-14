1ebd4c08d50aa2f7ffd154a0fd99ab79940ceadb
Santiago Aday <74830912+santiaday@users.noreply.github.com>
2022-06-14 10:46:21 -0700

    Add files via upload

--- converter_backend/src/main/java/com/example/Excel2Json/Excel2JsonApplication.java
+++ converter_backend/src/main/java/com/example/Excel2Json/Excel2JsonApplication.java
@@ -1,6 +1,13 @@
 package com.example.Excel2Json;
 
 import com.example.Excel2Json.property.FileStorageProperties;
+import com.github.opendevl.JFlat;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.streaming.SXSSFSheet;
+import org.apache.poi.xssf.streaming.SXSSFWorkbook;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
@@ -8,15 +15,93 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.stream.Stream;
+
+
+class Helper extends TimerTask {
+	public void run() {
+		File dir = new File("C:\\Users\\saaday\\Documents\\EG_excel_JSON_converter\\converter_backend\\springBootUploads");
+		File[] directoryListing = dir.listFiles();
+		if (directoryListing != null) {
+			for (File child : directoryListing) {
+				System.out.println(child.getName());
+				System.out.println(System.currentTimeMillis());
+				long lastModified = child.lastModified();
+				long epochTime = System.currentTimeMillis();
+				if((epochTime - lastModified) > 600000){
+					Path path = Paths.get(String.format("C:\\Users\\saaday\\Documents\\EG_excel_JSON_converter\\converter_backend\\springBootUploads\\%s", child.getName()));
+					try {
+						Files.delete(path);
+						System.out.println("Old files have been purged");
+					} catch (IOException e) {
+						throw new RuntimeException(e);
+					}
+				}
+			}
+		} else {
+
+		}
+	}
+}
+
 @SpringBootApplication
 @EnableConfigurationProperties({
 		FileStorageProperties.class
 })
 
 public class Excel2JsonApplication {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
+
+
 		SpringApplication.run(Excel2JsonApplication.class, args);
+
+		Timer timer = new Timer();
+		TimerTask task = new Helper();
+
+		timer.schedule(task , 1, 600000);
+
+//		String str = new String(Files.readAllBytes(Paths.get("C:\\Users\\saaday\\Documents\\EG_excel_JSON_converter\\converter_backend\\springBootUploads\\bb98f6b9-aea0-4be9-81fb-15193e5cdb34converted-Book1 - Copy (2) - Copy.json")));
+//
+//		JFlat flatMe = new JFlat(str);
+//
+////get the 2D representation of JSON document
+//		List<Object[]> json2csv = flatMe.json2Sheet().getJsonAsSheet();
+//
+////write the 2D representation in csv format
+//		flatMe.headerSeparator().write2csv("C:\\Users\\saaday\\Documents\\EG_excel_JSON_converter\\converter_backend\\springBootUploads\\bb98f6b9-aea0-4be9-81fb-15193e5cdb34converted-Book1 - Copy (2) - Copy.csv");
+//
+//		String csvFileAddress = "C:\\Users\\saaday\\Documents\\EG_excel_JSON_converter\\converter_backend\\springBootUploads\\bb98f6b9-aea0-4be9-81fb-15193e5cdb34converted-Book1 - Copy (2) - Copy.csv"; //csv file address
+//		String xlsxFileAddress = "C:\\Users\\saaday\\Documents\\EG_excel_JSON_converter\\converter_backend\\springBootUploads\\bb98f6b9-aea0-4be9-81fb-15193e5cdb34converted-Book1 - Copy (2) - Copy.xlsx"; //xlsx file address
+//		XSSFWorkbook workBook = new XSSFWorkbook();
+//		XSSFSheet sheet = workBook.createSheet("sheet1");
+//		String currentLine=null;
+//		int RowNum=0;
+//		BufferedReader br = new BufferedReader(new FileReader(csvFileAddress));
+//		while ((currentLine = br.readLine()) != null) {
+//			String strArr[] = currentLine.split(",");
+//			RowNum++;
+//			XSSFRow currentRow=sheet.createRow(RowNum);
+//			for(int i=0;i<strArr.length;i++){
+//				currentRow.createCell(i).setCellValue(strArr[i]);
+//			}
+//		}
+//
+//		FileOutputStream fileOutputStream =  new FileOutputStream(xlsxFileAddress);
+//		workBook.write(fileOutputStream);
+//		fileOutputStream.close();
+//		System.out.println("Done");
 	}
+
 
 	@Bean
 	public WebMvcConfigurer corsConfigurer() {
@@ -27,6 +112,8 @@ public class Excel2JsonApplication {
 			}
 		};
 	}
+
+
 
 
 }
--- converter_backend/src/main/java/com/example/Excel2Json/controller/FileController.java
+++ converter_backend/src/main/java/com/example/Excel2Json/controller/FileController.java
@@ -2,7 +2,13 @@ package com.example.Excel2Json.controller;
 
 import com.example.Excel2Json.payload.UploadFileResponse;
 import com.example.Excel2Json.service.FileStorageService;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.google.gson.Gson;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.*;
 import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
@@ -20,10 +26,13 @@ import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import javax.servlet.http.HttpServletRequest;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Iterator;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -37,12 +46,24 @@ public class FileController {
     private FileStorageService fileStorageService;
 
     @PostMapping("/excel/uploadFile/convert-to-single-json")
-    public void uploadFile(@RequestParam("file") MultipartFile file, @RequestParam("fileName")String fileName, @RequestParam("fileKey")String fileKey) {
-        String fileNameWOExt = fileName.substring(0 , fileName.lastIndexOf('.'));
-        EG_excel_to_single_JSON(file , fileNameWOExt, fileKey);
+    public void uploadFileToExcel(@RequestParam("file") MultipartFile file, @RequestParam("fileName")String fileName, @RequestParam("fileKey")String fileKey) {
+        String fileNameWOExt = fileName.substring(0, fileName.lastIndexOf('.'));
+        EG_excel_to_single_JSON(file, fileNameWOExt, fileKey);
 
     }
 
+    @PostMapping("/excel/uploadFile/convert-to-multiple-json")
+    public int uploadFileToMultipleJSON(@RequestParam("file") MultipartFile file, @RequestParam("fileName")String fileName, @RequestParam("fileKey")String fileKey) {
+        String fileNameWOExt = fileName.substring(0 , fileName.lastIndexOf('.'));
+        return EG_excel_to_multiple_JSON(file , fileNameWOExt, fileKey);
+    }
+
+    @PostMapping("/json/uploadFile/convert-to-excel")
+    public void uploadJSONToExcel(@RequestParam("file") MultipartFile file, @RequestParam("fileName")String fileName, @RequestParam("fileKey")String fileKey, @RequestParam("targetFile")String targetExtension) {
+        String sourceExtension = fileName.substring(fileName.lastIndexOf('.'));
+        EG_JSON_to_Excel(file , fileName, fileKey, targetExtension, sourceExtension);
+
+    }
 
     @GetMapping("/downloadFile/{fileName:.+}")
     public ResponseEntity<Resource> downloadFile(@PathVariable String fileName, HttpServletRequest request) {
@@ -70,15 +91,10 @@ public class FileController {
                 .body(resource);
     }
 
-    @GetMapping("")
-    public void testing(@PathVariable String fileName){
-        System.out.println(fileName);
-    }
-
-    private int counter = 1;
     private boolean multiple = false;
-    public void EG_excel_to_multiple_JSON(MultipartFile data, String name, String fileKey) {
+    public int EG_excel_to_multiple_JSON(MultipartFile data, String name, String fileKey) {
         multiple = true;
+        int counter = 1;
         try {
             XSSFWorkbook workBook = new XSSFWorkbook(data.getInputStream());
             XSSFSheet workSheet = workBook.getSheetAt(0);
@@ -93,19 +109,22 @@ public class FileController {
                     rowJsonObject.put(columnName, columnValue);
                 }
                 dataList.add(rowJsonObject);
-                EG_write_to_JSON(dataList, name, fileKey);
+                EG_write_to_JSON(dataList, name, fileKey, counter);
+                System.out.println(counter);
+                counter++;
                 dataList = new ArrayList<>();
             }
-            counter = 1;
+            return counter;
         } catch (IOException e) {
             e.printStackTrace();
         }
 
-        multiple=false;
+
+        return 1;
     }
 
     public String EG_excel_to_single_JSON(@RequestParam("data") MultipartFile data, String name, String fileKey) {
-        System.out.println("GOT HEREEEE");
+        multiple=false;
         try {
             XSSFWorkbook workBook = new XSSFWorkbook(data.getInputStream());
             XSSFSheet workSheet = workBook.getSheetAt(0);
@@ -121,8 +140,7 @@ public class FileController {
                 }
                 dataList.add(rowJsonObject);
             }
-            EG_write_to_JSON(dataList,name, fileKey);
-            counter = 1;
+            EG_write_to_JSON(dataList,name, fileKey, 0);
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -130,15 +148,111 @@ public class FileController {
         return "Success";
     }
 
-    public void EG_write_to_JSON(List<JSONObject> dataList, String name, String fileKey) {
+    private ObjectMapper mapper = new ObjectMapper();
+
+    public void EG_JSON_to_Excel(MultipartFile file, String fileName, String fileKey, String targetFileExtension, String sourceExtension) {
+
+        File srcFile = new File("C:\\Users\\saaday\\Documents\\EG_excel_JSON_converter\\converter_backend\\springBootUploads\\" + fileName);
+        System.out.println(sourceExtension);
+
+        if(sourceExtension.equals(".json")){
+            System.out.println("they do equal");
+        }
+        try {
+            if (!sourceExtension.equals(".json")) {
+                throw new IllegalArgumentException("The source file should be .json file only");
+            } else {
+                Workbook workbook = null;
+
+                //Creating workbook object based on target file format
+                if (targetFileExtension.equals(".xls")) {
+                    workbook = new HSSFWorkbook();
+                } else if (targetFileExtension.equals(".xlsx")) {
+                    workbook = new XSSFWorkbook();
+                } else {
+                    throw new IllegalArgumentException("The target file extension should be .xls or .xlsx only");
+                }
+
+                //Reading the json file
+                ObjectNode jsonData = (ObjectNode) mapper.readTree(srcFile);
+
+                //Iterating over the each sheets
+                Iterator<String> sheetItr = jsonData.fieldNames();
+                while (sheetItr.hasNext()) {
+
+                    // create the workbook sheet
+                    String sheetName = sheetItr.next();
+                    Sheet sheet = workbook.createSheet(sheetName);
+
+                    ArrayNode sheetData = (ArrayNode) jsonData.get(sheetName);
+                    ArrayList<String> headers = new ArrayList<String>();
+
+                    //Creating cell style for header to make it bold
+                    CellStyle headerStyle = workbook.createCellStyle();
+                    Font font = workbook.createFont();
+                    font.setBold(true);
+                    headerStyle.setFont(font);
+
+                    //creating the header into the sheet
+                    Row header = sheet.createRow(0);
+                    Iterator<String> it = sheetData.get(0).fieldNames();
+                    int headerIdx = 0;
+                    while (it.hasNext()) {
+                        String headerName = it.next();
+                        headers.add(headerName);
+                        Cell cell=header.createCell(headerIdx++);
+                        cell.setCellValue(headerName);
+                        //apply the bold style to headers
+                        cell.setCellStyle(headerStyle);
+                    }
+
+                    //Iterating over the each row data and writing into the sheet
+                    for (int i = 0; i < sheetData.size(); i++) {
+                        ObjectNode rowData = (ObjectNode) sheetData.get(i);
+                        Row row = sheet.createRow(i + 1);
+                        for (int j = 0; j < headers.size(); j++) {
+                            String value = rowData.get(headers.get(j)).asText();
+                            row.createCell(j).setCellValue(value);
+                        }
+                    }
+
+                    /*
+                     * automatic adjust data in column using autoSizeColumn, autoSizeColumn should
+                     * be made after populating the data into the excel. Calling before populating
+                     * data will not have any effect.
+                     */
+                    for (int i = 0; i < headers.size(); i++) {
+                        sheet.autoSizeColumn(i);
+                    }
+
+                }
+
+                //creating a target file
+                String filename = srcFile.getName();
+                filename = filename.substring(0, filename.lastIndexOf(".")) + targetFileExtension;
+                FileWriter targetFile = new FileWriter(String.format("C:\\Users\\saaday\\Documents\\EG_excel_JSON_converter\\converter_backend\\springBootUploads\\%s" , filename));
+
+                // write the workbook into target file
+                FileOutputStream fos = new FileOutputStream(String.valueOf(targetFile));
+                workbook.write(fos);
+
+                //close the workbook and fos
+                workbook.close();
+                fos.close();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void EG_write_to_JSON(List<JSONObject> dataList, String name, String fileKey, int counter) {
         Gson gson = new Gson();
         try {
-            FileWriter file = new FileWriter(String.format("C:\\Users\\saaday\\Documents\\EG_excel_JSON_converter\\converter_backend\\springBootUploads\\%s.json", multiple ? fileKey + "converted-" + name + "-" + counter : fileKey + "converted-" + name));
+            FileWriter file = new FileWriter(String.format("C:\\Users\\saaday\\Documents\\EG_excel_JSON_converter\\converter_backend\\springBootUploads\\%s.json",
+                    multiple ? fileKey + "converted-" + name + "-" + counter : fileKey + "converted-" + name));
             file.write(gson.toJson(dataList));
             file.close();
-            counter++;
         } catch (IOException e) {
-            System.out.println("theres been an error");
             e.printStackTrace();
         }
 
