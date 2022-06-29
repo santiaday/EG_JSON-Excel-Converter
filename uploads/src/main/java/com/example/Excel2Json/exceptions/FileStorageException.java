600a3f6193dd0e6f175ca5bca37cde8f5df22656
Santiago Aday <74830912+santiaday@users.noreply.github.com>
2022-06-29 08:54:26 -0700

    Worked on reverse functionality

---
+++ uploads/src/main/java/com/example/Excel2Json/exceptions/FileStorageException.java
@@ -0,0 +1,11 @@
+package com.example.Excel2Json.exceptions;
+
+public class FileStorageException extends RuntimeException {
+    public FileStorageException(String message) {
+        super(message);
+    }
+
+    public FileStorageException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
---
+++ uploads/src/main/java/com/example/Excel2Json/message/ResponseMessage.java
@@ -0,0 +1,14 @@
+package com.example.Excel2Json.message;
+
+public class ResponseMessage {
+    private String message;
+    public ResponseMessage(String message) {
+        this.message = message;
+    }
+    public String getMessage() {
+        return message;
+    }
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
